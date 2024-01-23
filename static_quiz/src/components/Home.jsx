ca0f2bc4aa3eaf331f44c79789888a5d925b8ccb
Satyam <satyamworks20@gmail.com>
2024-01-23 14:33:39 +0530

    Small Functioning Done

--- static_quiz/src/components/Home.jsx
+++ static_quiz/src/components/Home.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import './homeStyle.css'
+
 
 export default class Home extends Component {
   render() {
--- static_quiz/src/components/Quiz.jsx
+++ static_quiz/src/components/Quiz.jsx
@@ -1,24 +1,48 @@
 import React, { Component } from 'react'
-import "./qiuzStyle.css"
+import OurData from "/resources/quizQuestion"
 
 export default class Quiz extends Component {
+  constructor() {
+    super()
+  
+    this.state = {
+       count: 0
+    }
+  }
+
+  handleNext=()=>{
+    this.setState({
+      count : this.state.count>=14?this.state.count=1:this.state.count+1
+    })
+  }
+
+  handleBack=()=>{
+    this.setState({
+      count: this.state.count<=1?this.state.count=14:this.state.count-1
+    })
+  }
+
+  handleQuit=()=>{
+    alert("You are Exiting the Game")
+  }
+
   render() {
     return (
       <div className='outerDiv'>
         <div className='mainDiv' id='QuizDiv'>
             <h1 className='blackcolor'>Question</h1>
-            <p id='questionNo' className='blackcolor' >1 0f 15</p>
-            <p className='blackcolor'>Which is the only Monkey who can speek ?</p>
+            <p id='questionNo' className='blackcolor' >{this.state.count+1} 0f 15</p>
+            <p className='blackcolor'>{OurData[this.state.count].question}</p>
             <div className="optionsColumns">
-                <div className="options">Ananya</div>
-                <div className="options">Satyam</div>
-                <div className="options">Abhinandan</div>
-                <div className="options">Swasti</div>
+                <div className="options">{OurData[this.state.count].optionA}</div>
+                <div className="options">{OurData[this.state.count].optionB}</div>
+                <div className="options">{OurData[this.state.count].optionC}</div>
+                <div className="options">{OurData[this.state.count].optionD}</div>
             </div>
             <div className='bottonButton'>
-                <button>Present</button>
-                <button>Next</button>
-                <button>Quit</button>
+                <button onClick={this.handleBack}>Back</button>
+                <button onClick={this.handleNext}>Next</button>
+                <button onClick={this.handleQuit}>Quit</button>
             </div>
         </div>
       </div>
